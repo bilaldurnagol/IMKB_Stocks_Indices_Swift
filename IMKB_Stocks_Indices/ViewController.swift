e3f92f069f58c59cb05a0ea3492f5cbf4749247b
Bilal Durnagöl <bilaldurnagol@gmail.com>
2021-05-05 16:16:40 +0300

    Initial Commit

---
+++ IMKB_Stocks_Indices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMKB_Stocks_Indices
+//
+//  Created by Bilal Durnagöl on 5.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
